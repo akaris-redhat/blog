497fa387a57ec136e317f365f42e79a1cd2ed711
Andreas Karis <akaris@redhat.com>
2019-11-04 18:11:27 +0100

    .

--- virt-customize.sh
+++ virt-customize.sh
@@ -8,7 +8,9 @@ SIZE=$3
 cp $BASE $CLONE
 qemu-img resize $CLONE +${SIZE}
 
-virt-customize -a $CLONE --run-command 'sed -i -e "s/PasswordAuthentication no/PasswordAuthentication yes/g"  /etc/ssh/sshd_config'
+virt-customize -a $CLONE --run-command 'sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g"  /etc/ssh/sshd_config'
+virt-customize -a $CLONE --run-command 'sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g"  /etc/ssh/sshd_config'
 virt-customize -a $CLONE --run-command 'yum remove cloud-init* -y'
+virt-customize -a $CLONE --hostname ${CLONE/.qcow2/}
 virt-customize -a $CLONE --root-password password:password
 virt-customize -a $CLONE --selinux-relabel
